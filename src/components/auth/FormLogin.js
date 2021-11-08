cc3f27ef44c7867e9d8b98cd8814ce4a547c2ad2
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-11-08 01:20:02 -0300

    created FormLogin.js || Formik installed

---
+++ src/components/auth/FormLogin.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const FormLogin = () => {
+    return (
+     
+            <form className="form-group col-md-4">
+                    <div className="mb-3">
+                        <label for="email" className="form-label">Email address</label>
+                        <input 
+                            type="email" 
+                            className="form-control" 
+                            id="email" 
+                            aria-describedby="emailHelp"
+
+                        />
+                        
+                    </div>
+                    <div className="mb-3">
+                        <label for="Password" className="form-label">Password</label>
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            id="Password"
+
+                        />
+                    </div>
+                    <div className="d-flex justify-content-end">
+                        <button type="submit" className="btn btn-primary ">Login</button>
+                    </div>
+                </form>           
+        
+    )
+}
+
+export default FormLogin
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,23 +1,11 @@
 import React from 'react'
+import FormLogin from './FormLogin'
 
 const Login = () => {
     return (
         <div className="container">
-           <div className="h-100  d-flex justify-content-center align-items-center ">
-                <form className="form-group col-md-4">
-                    <div className="mb-3">
-                        <label for="email" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp"/>
-                        
-                    </div>
-                    <div className="mb-3">
-                        <label for="Password" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="Password"/>
-                    </div>
-                    <div className="d-flex justify-content-end">
-                        <button type="submit" className="btn btn-primary ">Login</button>
-                    </div>
-                </form>
+           <div className="h-100 d-flex justify-content-center align-items-center ">
+                <FormLogin/>
             </div>
         </div>
     )
