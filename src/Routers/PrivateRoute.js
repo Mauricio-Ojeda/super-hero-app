c000367eb54ef0cfd37744b5462fea7b8f936229
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-11-27 18:36:10 -0300

    logout clear local storage

---
+++ src/Routers/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route,  } from 'react-router-dom';
+
+const PrivateRoute = ({
+    isAuthenticated,
+    component: Component,
+    ...rest
+}) => {
+    return (
+        <Route { ...rest }
+            component={ (props) => (
+                ( isAuthenticated )
+                    ? ( <Component { ...props } /> )
+                    : ( <Redirect to="/login" /> )
+            )}
+        />
+    )
+}
+
+export default PrivateRoute
